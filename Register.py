f6c3d133d46a0af551cb44a3767a2f350e643553
Lucian <lucian.patrascoiu@yahoo.com>
2021-12-07 20:23:12 +0200

    first commit

---
+++ Register.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.chrome import options
+from selenium.webdriver.common import keys
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common import by
+import time
+
